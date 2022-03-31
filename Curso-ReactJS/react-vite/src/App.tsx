7f513262d1aab8099e8129cf05d7cccecf75cd13
Gustavo Marinho <gusttavomarinho@gmail.com>
2022-03-31 20:37:01 -0300

    expressoes jsx

--- Curso-ReactJS/react-vite/src/App.tsx
+++ Curso-ReactJS/react-vite/src/App.tsx
@@ -1,46 +1,20 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  // return React.createElement('div',null,'Meu nome é Gustavo');
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>Eai Gustavo Marinho!</p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+  let name: string = "Gustavo";
+  let n1: number = 10;
+  let n2: number = 3;
+
+  function somar(n1: number,n2: number): number{
+    return n1+n2
+  }
+
+  return(
+    // <div>Meu nome é {name} , ihuu</div>
+    <p>Soma: {somar(n1,n2)}</p>
   )
 }
 
-export default App
+
+export default App;
