34a192282bd548c040d3b37ea184c1f9be8892d1
Gustavo Marinho <gusttavomarinho@gmail.com>
2022-03-31 20:59:38 -0300

    separando componentes e usando

--- Curso-ReactJS/react-vite/src/App.tsx
+++ Curso-ReactJS/react-vite/src/App.tsx
@@ -1,18 +1,27 @@
+import { Header } from "./components/Header";
+
 
 const App = () => {
-  // return React.createElement('div',null,'Meu nome é Gustavo');
+  // return React.createElement('div',null,'Meu nome Ã© Gustavo');
 
   let name: string = "Gustavo";
   let n1: number = 10;
   let n2: number = 3;
+
+  let link: string = "https://gusttavodev.com";
 
   function somar(n1: number,n2: number): number{
     return n1+n2
   }
 
   return(
-    // <div>Meu nome é {name} , ihuu</div>
-    <p>Soma: {somar(n1,n2)}</p>
+    <div>
+      <Header/>
+      <div>Meu nome Ã© {name} , ihuu</div>
+      <p>Soma: {somar(n1,n2)}</p>
+      Link para o site:<br/>
+      <a href={link}>Clique aqui para acessar</a>
+    </div>
   )
 }
 
