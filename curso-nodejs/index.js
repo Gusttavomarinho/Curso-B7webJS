8103a88189fc4c0eadfea35446fc88a0dc306456
Gustavo Marinho <gusttavomarinho@gmail.com>
2023-06-30 15:05:42 -0300

    mais uma aula

--- curso-nodejs/index.js
+++
@@ -1,4 +0,0 @@
-let nome = 'Gustavo';
-let idade = 27;
-
-console.log(`Meu nome é ${nome} , idade: ${idade}`);
---
+++ curso-nodejs/src/index.ts
@@ -0,0 +1,4 @@
+let nome: string = 'Gustavo';
+let idade: number = 27;
+
+console.log(`Meu nome é ${nome} e eu tenho ${idade} anos`);
